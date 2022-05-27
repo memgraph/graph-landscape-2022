35e8c02f7458a9df1331c6e015ca95fdfa9d7a51
katarinasupe <supe.katarina@gmail.com>
2022-05-27 11:26:56 +0200

    Bump up gqlalchemy and fix duplicated edges

--- data_loader/app.py
+++ data_loader/app.py
@@ -1,11 +1,9 @@
+from ast import literal_eval
 from csv import reader
-from gqlalchemy import Memgraph
+from gqlalchemy import Memgraph, match
 from pathlib import Path
 import models
 import time
-
-
-memgraph = Memgraph()
 
 
 def load_data():
@@ -17,41 +15,73 @@ def load_data():
     with open(path) as read_obj:
         csv_reader = reader(read_obj)
         header = next(csv_reader)
+
         if header is None:
             return
+
         for row in csv_reader:
             # Create nodes and save them into Memgraph
             company = models.Company(
-                name=row[0], website=row[3], img="".join(row[0].split()).lower(),
+                name=row[0],
+                website=row[3],
+                img="".join(row[0].split()).lower(),
             ).save(memgraph)
-            category = models.Category(name=row[1]).save(memgraph)
-            subcategory = models.Subcategory(name=row[2]).save(memgraph)
 
-            # Create relationships and save them into Memgraph
-            models.IsPartOf(
-                _start_node_id=company._id, _end_node_id=subcategory._id
-            ).save(memgraph)
-            models.BelongsTo(
-                _start_node_id=subcategory._id, _end_node_id=category._id
-            ).save(memgraph)
+            models.Category(name=row[1]).save(memgraph)
+            models.Subcategory(name=row[2]).save(memgraph)
+
+            (
+                match()
+                .node(labels="Company", variable="company")
+                .where(item="company.name", operator="=", literal=row[0])
+                .with_(results={"company": "company"})
+                .match()
+                .node(labels="Subcategory", variable="subcategory")
+                .where(item="subcategory.name", operator="=", literal=row[2])
+                .merge()
+                .node(variable="company")
+                .to(edge_label="IS_PART_OF")
+                .node(variable="subcategory")
+                .execute()
+            )
+
+            (
+                match()
+                .node(labels="Subcategory", variable="subcategory")
+                .where(item="subcategory.name", operator="=", literal=row[2])
+                .with_(results={"subcategory": "subcategory"})
+                .match()
+                .node(labels="Category", variable="category")
+                .where(item="category.name", operator="=", literal=row[1])
+                .merge()
+                .node(variable="subcategory")
+                .to(edge_label="BELONGS_TO")
+                .node(variable="category")
+                .execute()
+            )
+
     print("Data has been loaded into Memgraph. Open Memgraph Lab to query it.")
 
 
 def connect_to_memgraph():
     """Establish a connection to the database."""
-
+    memgraph = Memgraph()
     connection_established = False
+
     while not connection_established:
         try:
             if memgraph._get_cached_connection().is_active():
                 connection_established = True
+                return memgraph
         except:
             print("Memgraph probably isn't running.")
             time.sleep(4)
 
 
+memgraph = connect_to_memgraph()
+
+
 def main():
-    connect_to_memgraph()
     load_data()
 
 
